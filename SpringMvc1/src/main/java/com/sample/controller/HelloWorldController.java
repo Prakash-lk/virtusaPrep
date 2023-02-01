274995cb99da6fad423754c63a144157ba4100a8
Prakash L <111338319+Prakash-lk@users.noreply.github.com>
2023-02-01 09:41:03 +0530

    Add files via upload

---
+++ SpringMvc1/src/main/java/com/sample/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/")
+public class HelloWorldController {
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView welcomeMsg() {
+		return new ModelAndView("welcome","msg","Hello Buddy !!!");
+	}
+}
